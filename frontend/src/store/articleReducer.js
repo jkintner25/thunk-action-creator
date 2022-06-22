39794d9b6bea7c983f6ecf0e35d81815324f10c2
jvos <john.voskuyl@gmail.com>
2022-06-22 09:50:51 -0700

    minor edit

--- frontend/src/store/articleReducer.js
+++ frontend/src/store/articleReducer.js
@@ -3,7 +3,7 @@ import articles from '../data/data.json';
 const LOAD_ARTICLES = 'article/loadArticles';
 const ADD_ARTICLE = 'article/addArticle';
 
-export const loadArticles = () => {
+export const loadArticles = (articles) => {
   return {
     type: LOAD_ARTICLES,
     articles
